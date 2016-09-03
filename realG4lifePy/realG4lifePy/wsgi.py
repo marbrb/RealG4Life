51f1c0341b9623809a0c3653cab677dd6de53590
Miguel Rodriguez <marbrb1@gmail.com>
2016-09-02 19:03:36 -0500

    first commit base project files

---
+++ realG4lifePy/realG4lifePy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for realG4lifePy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realG4lifePy.settings")
+
+application = get_wsgi_application()
