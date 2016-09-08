fe76e366d60279cf1922ddc5f91e959b20a02c9b
Miguel Rodriguez <marbrb1@gmail.com>
2016-09-07 20:19:44 -0500

    added node_api view

--- realG4lifePy/core/views.py
+++ realG4lifePy/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt   #Skip the csrf middleware protection
 from django.http import HttpResponse, HttpResponseServerError
 from django.contrib.sessions.models import Session
+from core.models import User
+import pika
 
 def home(request):
     #session = Session.objects.get(session_key=request.POST.get('sessionid'))
@@ -11,4 +13,28 @@ def home(request):
 
 @csrf_exempt    #ignorara la proteccion csrf
 def node_api(request):
-    session = Session.objects.all.get(session)
+    try:
+        #Get user from session ID
+        session = Session.objects.all.get(session_key=request.POST.get('sessionid'))
+        userID  = session.get_decoded().get('_auth_user_id')
+        user = User.objects.get(id=userID)
+
+        # Comments.objects.create(user=user)
+
+        temp_comment = request.POST.get('comment')
+
+        #RabbitMQ
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+        #create recipient queue
+        channel.queue_declare(queue='culo')
+
+        #send message to 'culo' recipient
+        channel.basic_publish(exchange='', routing_key='culo', body=temp_comment)
+
+        connection.close()
+
+        return HttpResponse("Todo nice gente.")
+
+    except:
+        return HttpResponseServerError(str(e))
